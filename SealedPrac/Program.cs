8160168870166320049955508ebf299dd1da6053
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-16 10:15:29 +0900

    초기 커밋.

---
+++ SealedPrac/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedPrac
+{
+    class Program
+    {
+        class Parent
+        {
+            public virtual void Method() { }
+        }
+        class Child : Parent
+        { 
+            public sealed override void Method() { }
+        }
+        class GrandChild : Child
+        {
+            public override void Method() { }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
